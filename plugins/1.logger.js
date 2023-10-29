a521404c34f6b2dd78fcf235a88c9eb4531b0c47
markthree <1801982702@qq.com>
2023-10-29 14:53:18 +0800

    refactor(plugins/logger): abstract log util

--- plugins/1.logger.js
+++ plugins/1.logger.js
@@ -1,56 +1,29 @@
-import { existsSync } from "fs";
-import { dirname, resolve } from "path";
-import { appendFile, mkdir } from "fs/promises";
+import { resolve } from "path";
 
-async function writeLog(file, text) {
-  if (!existsSync(file)) {
-    await mkdir(dirname(file), {
-      recursive: true,
-    });
-  }
-
-  return appendFile(file, `${text}\n`, { encoding: "utf-8" });
-}
-
-// 打印日志
-export default defineNitroPlugin(async (nitroApp) => {
-  const timeRtf = new Intl.DateTimeFormat("zh-cn", {
-    dateStyle: "short",
-    timeStyle: "short",
-  });
-
-  const dateRtf = new Intl.DateTimeFormat("zh-cn", {
-    dateStyle: "short",
-  });
-
+// 日志记录
+export default defineNitroPlugin((nitroApp) => {
   const { logsDir } = useAppConfig();
 
   nitroApp.hooks.hook("request", (event) => {
-    const now = Date.now();
+    const { now, ip, time, date } = createLogInfo(event);
     event.context.startTime = now;
-    const ip = getRequestIP(event, { xForwardedFor: true }) ?? "";
-    const date = timeRtf.format(now);
-    const record = `${date} --> ${event.path} ${ip}`;
+    const record = `${time} --> ${event.path} ${ip}`;
     console.log(record);
-    writeLog(resolve(logsDir, dateRtf.format(now)), record);
+    writeLog(resolve(logsDir, date), record);
   });
 
   nitroApp.hooks.hook("afterResponse", (event) => {
-    const now = Date.now();
-    const time = now - event.context.startTime;
-    const ip = getRequestIP(event, { xForwardedFor: true }) ?? "";
-    const date = timeRtf.format(now);
-    const record = `${date} <-- ${event.path} ${ip} ${time}ms`;
+    const { now, ip, time, date } = createLogInfo(event);
+    const duration = now - event.context.startTime;
+    const record = `${time} <-- ${event.path} ${ip} ${duration}ms`;
     console.log(record);
-    writeLog(resolve(logsDir, dateRtf.format(now)), record);
+    writeLog(resolve(logsDir, date), record);
   });
 
   nitroApp.hooks.hook("error", async (error, { event }) => {
-    const now = Date.now();
-    const ip = getRequestIP(event, { xForwardedFor: true }) ?? "";
-    const date = timeRtf.format(now);
-    const record = `${date} ${event.path} ${ip} ${error}`;
+    const { ip, time, date } = createLogInfo(event);
+    const record = `${time} ${event.path} ${ip} ${error}`;
     console.error(record);
-    writeLog(resolve(`${logsDir}/error`, dateRtf.format(now)), record);
+    writeLog(resolve(`${logsDir}/error`, date), record);
   });
 });
---
+++ utils/log.js
@@ -0,0 +1,30 @@
+import { dirname } from "path";
+import { existsSync } from "fs";
+import { appendFile, mkdir } from "fs/promises";
+
+export async function writeLog(file, text) {
+  if (!existsSync(file)) {
+    await mkdir(dirname(file), {
+      recursive: true,
+    });
+  }
+  return appendFile(file, `${text}\n`, { encoding: "utf-8" });
+}
+
+const timeRtf = new Intl.DateTimeFormat("zh-cn", {
+  dateStyle: "short",
+  timeStyle: "short",
+});
+
+const dateRtf = new Intl.DateTimeFormat("zh-cn", {
+  dateStyle: "short",
+});
+
+export function createLogInfo() {
+  const now = Date.now();
+  const event = useEvent();
+  const time = timeRtf.format(now);
+  const date = dateRtf.format(now);
+  const ip = getRequestIP(event, { xForwardedFor: true }) ?? "";
+  return { now, ip, time, date };
+}
